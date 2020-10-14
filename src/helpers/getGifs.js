53e429c69b9ef3d9717e2e9da89d62c75614193a
LuisV <luis.vlladiego@blackfish.com.co>
2020-10-14 08:16:41 -0500

    aumenta limite de busqueda

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifs = async(category) =>{
     const apiKey = "pofXeUiZ6aI5pDrymWwmn9UMy9Y0heEt";
-    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category)}&api_key=${apiKey}&limit=6&offset=0&rating=g&lang=en`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category)}&api_key=${apiKey}&limit=10&offset=0&rating=g&lang=es`;
     const resp = await fetch(url);
     const {data} = await resp.json();
     const gifs = data.map(img=>{
